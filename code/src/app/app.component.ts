817eeaf34a675c122963453d638bb30dea075a5b
abhiramTS <abhiramts007@gmail.com>
2021-12-05 11:33:44 +0530

    WIP

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -11,6 +11,11 @@ import Two from 'two.js';
 import { sequenceGeneratorFunction } from './service/generator';
 import { SequenceGeneratorService } from './service/sequence-generator.service';
 
+enum type {
+  SolidAndStroke,
+  Solid,
+  Stroke,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,6 +33,15 @@ import { SequenceGeneratorService } from './service/sequence-generator.service';
   ],
 })
 export class AppComponent implements OnInit, AfterViewInit {
+  pointSize = 3;
+  gap = 3;
+  pitch = this.pointSize + this.gap;
+  typeOfDesign: type = type.SolidAndStroke;
+
+  get type() {
+    return type;
+  }
+
   title = 'abhiramTS';
   sequence$: Observable<number>;
   // colors$: Observable<string>;
@@ -44,6 +58,9 @@ export class AppComponent implements OnInit, AfterViewInit {
   cursor: any;
   bgColor: string = '#fff';
   points: number[] = [];
+  paths: any[] = [];
+
+  backgroundStyle: any;
 
   constructor(private sequenceGenerator: SequenceGeneratorService) {
     this.sequence$ = this.sequenceGenerator.getSequence$();
@@ -56,21 +73,12 @@ export class AppComponent implements OnInit, AfterViewInit {
       autostart: true,
     }).appendTo(this.box.nativeElement);
     this.drawCurve();
-    setInterval(() => {
+    /* setInterval(() => {
       this.refresh();
-    }, 1000);
+    }, 1000); */
   }
   private refresh() {
-    this.bgColor =
-      '#' +
-      Array(3)
-        .fill(1)
-        .map((e) =>
-          Math.round(Math.random() * 245)
-            .toString(16)
-            .padStart(2, '0')
-        )
-        .join('');
+    this.bgColor = this.generateRandomColor();
     this.two.clear();
     this.curveRefresh(this.points, window.innerHeight);
   }
@@ -78,14 +86,17 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   drawCurve() {
+    var { circleEndPoints, height } = this.refreshPoints();
+    this.curveRefresh(circleEndPoints, height);
+  }
+
+  private refreshPoints() {
     const height = window.innerHeight;
     const width = window.innerWidth;
-    const pointSize = 5;
-    const gap = 10;
     const gen = sequenceGeneratorFunction();
     let i = gen.next().value;
     let circleEndPoints = [];
-    while (width / (pointSize + gap) > i) {
+    while (width / (this.pointSize + this.gap) > i) {
       circleEndPoints.push(i);
       i = gen.next().value;
     }
@@ -93,42 +104,52 @@ export class AppComponent implements OnInit, AfterViewInit {
     i = gen.next().value;
 
     this.points = circleEndPoints;
-    this.curveRefresh(circleEndPoints, height);
+    return { circleEndPoints, height };
   }
 
   private curveRefresh(circleEndPoints: number[], height: number) {
-    const paths: any[] = [];
-    circleEndPoints /* .slice(0,5) */
-      .forEach((e, i, a) => {
-        if (i != 0) {
-          const circlePoints: any = [];
-          const r = (15 * (e - a[i - 1])) / 2;
-          circlePoints.push(
-            ...this.circlePoints(height, r, a[i - 1] * 15, i % 2 == 1)
-          );
-          const circle = this.two.makePath(circlePoints, true);
-          /* circle.start = 0;
+    (this.paths ?? []).forEach((e) => (e = null));
+    this.paths = [];
+    const paths: any[] = this.paths;
+    circleEndPoints.forEach((e, i, a) => {
+      if (i != 0) {
+        const circlePoints: any = [];
+        const r = (this.pitch * (e - a[i - 1])) / 2;
+        circlePoints.push(
+          ...this.circlePoints(height, r, a[i - 1] * this.pitch, i % 2 == 1)
+        );
+        const circle = this.two.makePath(circlePoints, true);
+        /* circle.start = 0;
           circle.ending = 0; */
-          const color =
-            '#' +
-            Array(3)
-              .fill(1)
-              .map((e) =>
-                Math.round(Math.random() * 245)
-                  .toString(16)
-                  .padStart(2, '0')
-              )
-              .join('');
-          const test = Math.round(Math.random() * 1000);
-          if (test >= 100 && test < 200) {
-            circle.fill = color;
-          } else {
-            circle.noFill().linewidth = 5 + Math.round(Math.random() * 8);
-            circle.stroke = color;
+        const color = this.generateRandomColor();
+        switch (this.typeOfDesign) {
+          case type.Solid:
+            {
+              circle.fill = color;
+            }
+            break;
+          case type.Stroke:
+            {
+              circle.noFill().linewidth =
+                this.pointSize + Math.round(Math.random() * this.pointSize * 2);
+              circle.stroke = color;
+            }
+            break;
+          case type.SolidAndStroke:
+          default: {
+            const test = Math.round(Math.random() * 1000);
+            if (test >= 100 && test < 200) {
+              circle.fill = color;
+            } else {
+              circle.noFill().linewidth =
+                this.pointSize + Math.round(Math.random() * this.pointSize * 2);
+              circle.stroke = color;
+            }
           }
-          paths.push(circle);
         }
-      });
+        paths.push(circle);
+      }
+    });
     // this.draw(paths);
   }
 
@@ -185,5 +206,32 @@ export class AppComponent implements OnInit, AfterViewInit {
     const x = r * Math.cos(theta) + R + xOffset;
     const y = r * Math.sin(theta) + yOffset;
     return new Two.Anchor(x, y);
+  }
+
+  calculatePitch() {
+    this.pitch = (this.gap ?? 0) + (this.pointSize ?? 0);
+    this.refreshPoints();
+  }
+
+  private changeBackgroundStyle() {
+    const angle = Math.round(Math.random() * 360);
+    const noOfColors = Math.round(Math.random() * 4) + 1;
+    const colors = Array(noOfColors)
+      .fill('')
+      .map((e) => e + this.generateRandomColor(true));
+  }
+
+  private generateRandomColor(rgba: boolean = false): string {
+    return (
+      '#' +
+      Array(rgba ? 4 : 3)
+        .fill(1)
+        .map((e) =>
+          Math.round(Math.random() * 245)
+            .toString(16)
+            .padStart(2, '0')
+        )
+        .join('')
+    );
   }
 }
--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
@@ -12,7 +12,9 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ScullyLibModule
+    ScullyLibModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
